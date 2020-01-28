1d983b74a85b4aa5dc8820530fedf0e54befa3a5
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-01-28 20:24:05 +0100

    Obfuscate js code

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,13 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const JavaScriptObfuscator = require("webpack-obfuscator");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "none",
+  plugins: [
+    new JavaScriptObfuscator({
+      rotateUnicodeArray: true,
+    }),
+  ],
 });
