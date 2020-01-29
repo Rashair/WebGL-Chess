3f91a0c175a7c8d4c8bced582cf849f101574036
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2020-01-29 09:00:15 +0100

    Add obfuscation options

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,23 @@ module.exports = merge(common, {
   devtool: "none",
   plugins: [
     new JavaScriptObfuscator({
-      rotateUnicodeArray: true,
-    }),
-  ],
+      compact: true,
+      controlFlowFlattening: false,
+      deadCodeInjection: false,
+      debugProtection: false,
+      debugProtectionInterval: false,
+      disableConsoleOutput: true,
+      identifierNamesGenerator: "hexadecimal",
+      log: false,
+      renameGlobals: false,
+      rotateStringArray: true,
+      selfDefending: true,
+      shuffleStringArray: true,
+      splitStrings: false,
+      stringArray: true,
+      stringArrayEncoding: false,
+      stringArrayThreshold: 0.75,
+      unicodeEscapeSequence: false
+    })
+  ]
 });
